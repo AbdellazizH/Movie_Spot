04bec89a0d2fdd02cd078b4242fc51578c2337bb
Aziz21120 <doukali_fr@live.fr>
2025-11-01 18:47:35 +0100

    Fifth commit

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import landing_page
+from .views import landing_page, movie_detail
 
 urlpatterns = [
     path('', landing_page, name='landing_page'),
+    path('movie/<int:movie_id>', movie_detail, name='movie_detail'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
+from django.contrib.auth import login
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import FormView
+from users.forms import RegisterUserForm
 
-# Create your views here.
+
+class Login(LoginView):
+    template_name = "users/accounts/login.html"
+    next_page = "/"
+
+
+class Logout(LogoutView):
+    next_page = "/"
+
+
+class RegisterUser(FormView):
+    template_name = "users/accounts/register.html"
+    form_class = RegisterUserForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
