4939717d1c749aaec98cf8cb434a2a6748539871
Aziz21120 <doukali_fr@live.fr>
2025-11-02 20:21:01 +0100

    Sixth commit

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from users.models import CustomUser
+from users.models import CustomUser, UserList, ListItem
 
 
 admin.site.register(CustomUser, UserAdmin)
+
+
+class ListItemInline(admin.TabularInline):
+    model = ListItem
+
+
+@admin.register(UserList)
+class UserListAdmin(admin.ModelAdmin):
+    inlines = [ListItemInline]
+
+
+@admin.register(ListItem)
+class ListItemAdmin(admin.ModelAdmin):
+    pass
--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,16 @@
 from django.contrib.auth.forms import UserCreationForm
-from users.models import CustomUser
+from django.forms import forms, ModelForm
+
+from users.models import CustomUser, UserList
 
 
 class RegisterUserForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class CreateListForm(ModelForm):
+    class Meta:
+        model = UserList
+        fields = ('name',)
